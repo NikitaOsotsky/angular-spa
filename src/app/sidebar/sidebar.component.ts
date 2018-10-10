7351181ce0e2eb90403bc032dd10732e3f30e769
Mikita Asotski <masotski@exadel.com>
2018-10-10 12:37:08 +0300

    move http requests to separate file service;

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpService} from '../http.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  providers: [HttpService]
 })
 export class SidebarComponent implements OnInit {
   public menu: Array<string> = ['Please, wait...'];
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpService) {
   }
 
   ngOnInit() {
-    this.http.get('http://localhost:3005/menu').subscribe((data: Array<string>) => this.menu = data);
+    this.http.getData().subscribe((data: Array<string>) => this.menu = data);
   }
 
 }
