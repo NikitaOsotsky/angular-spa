1d4b937dbec9f4bfb590aa3e85de2b2429d470e7
Mikita Asotski <masotski@exadel.com>
2018-10-11 12:41:23 +0300

    add throwing error to app-error;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-spa';
+  public error: string;
+
+  public errorHandler(event: any) {
+    this.error = event.message;
+  }
 }
