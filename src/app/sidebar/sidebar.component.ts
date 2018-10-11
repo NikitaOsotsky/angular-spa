684c3ae54418d20957ab7e4276dccddfa2146367
Mikita Asotski <masotski@exadel.com>
2018-10-11 12:39:05 +0300

    add error trow; fix some bugs;

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { HttpService} from '../http.service';
 
 @Component({
@@ -7,19 +7,25 @@ import { HttpService} from '../http.service';
   styleUrls: ['./sidebar.component.css'],
   providers: [HttpService]
 })
+
 export class SidebarComponent implements OnInit {
+
+  @Output() error: EventEmitter<any> = new EventEmitter();
   public menu: Array<string>;
   protected isLoaded: boolean;
   constructor(private http: HttpService) {
   }
+
   public getRout(item) {
     if (item.toLowerCase() === 'home') { return ''; }
     return ('/' + item.toLowerCase());
   }
+
   ngOnInit() {
     this.http.getData().subscribe((data: Array<string>) => {
       this.menu = data;
       this.isLoaded = true;
-    });
+    },
+      (error: any) => {console.log(error); this.error.emit(error);  this.isLoaded = true; } );
   }
 }
