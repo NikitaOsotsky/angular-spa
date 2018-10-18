96698094fc34c2500cb9e4a8a2e96036235cc7b1
Mikita Asotski <masotski@exadel.com>
2018-10-18 15:04:19 +0300

    add input data for pagination;

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
+  /**
+   * object[], of filteredNews
+   */
+  @Input() blocks: object[];
+  /**
+   * number, items count on one page
+   */
+  @Input() itemsOnPage: number;
 
   constructor() { }
 
