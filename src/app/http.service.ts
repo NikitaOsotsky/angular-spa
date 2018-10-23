2906d1a2ffb6b76548c1048ecfcab2bad1193417
Mikita Asotski <masotski@exadel.com>
2018-10-23 17:40:46 +0300

    remake getting data way with switching for whan component we need data;;

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -3,11 +3,17 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class HttpService {
-
+  private newsLink = 'http://172.20.132.174:3005/news';
+  private menuLink = 'http://172.20.132.174:3005/menu';
   constructor(private http: HttpClient) { }
 
-  public getData(url: string) {
-    url = (url || 'http://172.20.132.174:3005/menu');
+  public getData(componentName: string) {
+    let url: string;
+    switch (componentName) {
+      case 'news': url = this.newsLink; break;
+      case 'menu': url = this.menuLink; break;
+      default: url = undefined;
+    }
     return this.http.get(url);
   }
 }
