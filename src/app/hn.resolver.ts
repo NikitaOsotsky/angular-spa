8a33c9610868079ed67ee0e81565c54b068ba69d
Mikita Asotski <masotski@exadel.com>
2018-10-31 15:01:32 +0300

    add new logic to resolver; now it can to disabled spinService;

--- src/app/hn.resolver.ts
+++ src/app/hn.resolver.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
-import { Observable, of } from 'rxjs';
-import {HttpService} from './http.service';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { HttpService } from './http.service';
+import { SpinService } from './shared/spin/spin.service';
 
 @Injectable()
 export class HnResolver implements Resolve<any> {
   private data: any;
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private SpService: SpinService) {
  }
 
   resolve(route: ActivatedRouteSnapshot) {
@@ -14,6 +14,13 @@ export class HnResolver implements Resolve<any> {
     this.httpService.getData(name, false).subscribe((data: Array<object>) => {
       this.data = data;
     });
-    return this.data || this.httpService.getData(name, true);
+    return this.data || this.httpService.getData(name, true).toPromise().then( (response: any) => {
+      this.SpService.disActivateModalSpinner();
+      return response;
+    }).catch((error) => {
+      console.log(error);
+      this.SpService.disActivateModalSpinner();
+      return [];
+    });
   }
 }
