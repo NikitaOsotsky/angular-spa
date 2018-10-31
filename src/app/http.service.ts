1ae4c73b9c50530d55b43f7152f4194d87a44e37
Mikita Asotski <masotski@exadel.com>
2018-10-31 15:03:33 +0300

    little fixes in http.service;

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,15 +1,13 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class HttpService {
   private newsLink = 'http://172.20.132.174:3005/news';
   private menuLink = 'http://172.20.132.174:3005/menu';
-  private needSpinner = false;
   constructor(private http: HttpClient) { }
 
   public getData(componentName: string, needSpinner: boolean) {
-    if (needSpinner === true) { this.needSpinner = true; }
     let url: string;
     switch (componentName) {
       case 'news': url = this.newsLink; break;
@@ -18,7 +16,7 @@ export class HttpService {
     }
 
     const params = new HttpParams();
-    params['needSpinner'] = this.needSpinner;
+    params['needSpinner'] = needSpinner;
     return this.http.get(url, {params: params});
   }
 }
