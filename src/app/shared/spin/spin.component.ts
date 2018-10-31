c2f25f2cf7aff22e6fe322cf71004e32777f96c9
Mikita Asotski <masotski@exadel.com>
2018-10-31 14:57:04 +0300

    remade spinComponent for managin from service;

--- src/app/shared/spin/spin.component.ts
+++ src/app/shared/spin/spin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { SpinService } from './spin.service';
 
 @Component({
   selector: 'app-spin',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./spin.component.css']
 })
 export class SpinComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @Output() isSpinnerActive: EventEmitter<any> = new EventEmitter();
+  constructor( private SpService: SpinService ) {
+    this['instance'] = Math.random();
   }
 
+  ngOnInit() {
+    console.log('init in spin.component', this); /*test*/
+    this.SpService.componentData.subscribe((data) => {
+      console.log('subscribe in spin.component', data); /*test*/
+      this.changeSpinnerActivity(data);
+    });
+  }
+
+  public changeSpinnerActivity(data: boolean) {
+    this.isSpinnerActive.emit(data);
+  }
 }
